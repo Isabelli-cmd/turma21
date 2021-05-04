456470d0919ae352abc10914f6714c0673593be3
isabelli-cmd <isabellisantos2019@gmail.com>
2021-05-04 17:01:51 -0300

    JAVA

---
+++ java/AULAS/src/exemplos/temperatura.java
@@ -0,0 +1,26 @@
+package exemplos;
+
+import java.util.Scanner;
+
+	public class temperatura {
+		public static void main (String [] args) {
+		
+		Scanner leia = new Scanner (System.in);
+		
+	//variaveis
+		String nome;
+		double F = 0.0;
+		double C = 0.0;
+		
+		//entrada
+		System.out.println("Qual a temperatura em F? ");
+		F = leia.nextDouble();
+		
+		//processamento
+		C = (F - 32) / 1.8;
+		
+		//saida
+		System.out.println("A temperatura em C é: "+C);
+		
+		}
+	}
---
+++ java/AULAS/src/exercicios/ex1.java
@@ -0,0 +1,10 @@
+package exercicios;
+
+public class ex1 {
+	//funçao sem retorno
+		public static void main (String args[])
+		{
+			System.out.println("Hello Generation");
+		}
+
+}
